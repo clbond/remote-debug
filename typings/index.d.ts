cbec74e63e4c8a0c84b9eaba8b8a4616f387db18
Chris Bond <chris.bond@rangle.io>
2016-07-29 11:39:44 -0400

    typings should not be part of repo

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/isomorphic-fetch/index.d.ts" />
-/// <reference path="globals/node-fetch/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/winston/index.d.ts" />
-/// <reference path="globals/ws/index.d.ts" />
